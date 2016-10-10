0d5981dc8125735ed0ae196acee7187947066b27
alazareva <a.lazareva@gmail.com>
2016-10-09 21:15:10 -0700

    removing old tests

--- ecnn/tests/test_tensorflowModel.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase
-
-
-class TestTensorflowModel(TestCase):
-    # TODO
-    # create simple model in ipython, train, save weights, run accuracy and loss to compare
-    # load model in again and train again to compare weigts, bisases,
-    def test_run_model(self):
-        pass
