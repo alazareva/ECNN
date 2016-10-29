63d221f6e4855e8d9d6dc756dae40c07da151605
alazareva <a.lazareva@gmail.com>
2016-10-29 14:34:01 -0700

    adding report

--- ecnn/__main__.py
+++ ecnn/__main__.py
@@ -8,7 +8,7 @@ import ecnn.functions as functions
 def main():
     dataset = ds.get_cifar_10_dataset()
     dataset.limit_training_set_size(10000)
-    tournament = tmnt.Tournament() # maybe pass in defaults here?
+    tournament = tmnt.Tournament()
     fitness_function = functions.sort_on_accuracy
     tournament.run(dataset, fitness_function)
 
